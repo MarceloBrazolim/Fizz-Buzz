96bbc0958a9eca33e3fa69f47934a491325c28b1
MarceloBrazolim <marcelobrazolim@gmail.com>
2021-03-27 23:37:03 -0300

    Create FizzBuzz.js

---
+++ JavaScript/FizzBuzz.js
@@ -0,0 +1,12 @@
+for (x = 1; x < 100; x++) {
+  var fb = "";
+  if (x % 3 == 0) {
+    fb = "Fizz";
+  }
+  if (x % 5 == 0) {
+    fb += "Buzz";
+  }
+  if (!fb) {
+    console.log(x);
+  } else console.log(fb);
+}
