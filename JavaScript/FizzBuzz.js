d7dceba59a68db773986b6694b865ebe3c6d071a
MarceloBrazolim <marcelobrazolim@gmail.com>
2021-03-29 12:01:17 -0300

    Update FizzBuzz.js

--- JavaScript/FizzBuzz.js
+++ JavaScript/FizzBuzz.js
@@ -2,7 +2,5 @@ for (x = 1; x < 101; x++) {
   let fb = "";
   if (x % 3 == 0) fb = "Fizz";
   if (x % 5 == 0) fb += "Buzz";
-  if (!fb) {
-    console.log(x);
-  } else console.log(fb);
+  console.log(fb || x);
 }
